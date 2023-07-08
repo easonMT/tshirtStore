270a464b3ca317da378ce59df789d96a379bd781
easonMT <t.eason@lafilm.edu>
2023-07-07 22:12:10 -0400

    created product class. added instance variables

--- storeFront/Product.java
+++ storeFront/Product.java
@@ -1,3 +1,21 @@
 package storeFront;
 
+public class Product {
+    
+    private int id;
+    private String name;
+    private double price;
 
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
