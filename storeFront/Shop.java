f95d8a73a96d65e067365b4d96ac69fbd2f817b2
easonMT <t.eason@lafilm.edu>
2023-07-07 22:37:51 -0400

    added shop class and added methods

---
+++ storeFront/Shop.java
@@ -0,0 +1,27 @@
+package storeFront;
+
+import  java.util.ArrayList;
+
+public class Shop {
+    
+    private String name;
+
+    private ArrayList<Product> products = new ArrayList<>();
+
+    public String getName(){
+        return name;
+    }
+
+    public void printProducts() {
+
+    }
+
+    public int findProduct() {
+        return 0;
+    }
+
+    public Product getProductById(int id) {
+        return null;
+    }
+
+}
