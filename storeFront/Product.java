2a63362efb755b131cdfa3cb2952b03ed7de0f92
easonMT <t.eason@lafilm.edu>
2023-07-07 22:29:59 -0400

    made adjustments to the product class and added a constructor

--- storeFront/Product.java
+++ storeFront/Product.java
@@ -6,6 +6,12 @@ public class Product {
     private String name;
     private double price;
 
+    public Product(int id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
@@ -18,15 +24,4 @@ public class Product {
         return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
