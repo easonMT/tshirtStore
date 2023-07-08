9dcee7c8df04c67c6b14a0b052d880e0debfe558
easonMT <t.eason@lafilm.edu>
2023-07-07 22:37:24 -0400

    added functionality to the methods in cart class

--- storeFront/Cart.java
+++ storeFront/Cart.java
@@ -5,22 +5,31 @@ import java.util.ArrayList;
 public class Cart {
     
     private ArrayList<Product> items = new ArrayList<>();
-    private double total;
 
     public void showItems(){
+        System.out.println("Items in Cart:");
+        for (Product item : items) {
+            System.out.println(item.getName() + " - $" + item.getPrice());
+        }
 
     }
 
     public void addItem(Product p) {
+        items.add(p);
 
     }
 
-    public void removeItem() {
-
+    public void removeItem(Product p) {
+        items.remove(p);
     }
 
-    public void calculateTotal(){
-        
+    public double calculateTotal(){
+        double totalPrice = 0.0;
+        for (Product itemPrice : items) {
+            totalPrice += itemPrice.getPrice();
+        }
+        return totalPrice;
+
     }
 
 
