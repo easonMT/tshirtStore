3dd6a84a5f17e4d63865cb5ffc9c644e825bb853
easonMT <t.eason@lafilm.edu>
2023-07-07 22:21:16 -0400

    created cart class to add, remove, show items, calculate total

---
+++ storeFront/Cart.java
@@ -0,0 +1,27 @@
+package storeFront;
+
+import java.util.ArrayList;
+
+public class Cart {
+    
+    private ArrayList<Product> items = new ArrayList<>();
+    private double total;
+
+    public void showItems(){
+
+    }
+
+    public void addItem(Product p) {
+
+    }
+
+    public void removeItem() {
+
+    }
+
+    public void calculateTotal(){
+        
+    }
+
+
+}
