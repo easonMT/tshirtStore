fbb1a8a9bedb650d3371f8969f492aff0c7dc0db
easonMT <t.eason@lafilm.edu>
2023-07-09 22:12:43 -0400

    added products as well as product methods to shop class

--- storeFront/Shop.java
+++ storeFront/Shop.java
@@ -2,5 +2,26 @@ package storeFront;
 
 
 public class Shop {
+    private String[] products = {"Crew Neck", "V-Neck", "Polo", "Boat Neck", "Tank Top"};
+
+    public Shop() {
+
+    }
+
+        private void printProducts() {
+        System.out.println("--Products--");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println("ID " +i+": " + products[i]);
+        }
+    }
+
+    public int findProduct(String searchText) {
+        for (int i = 0; i < products.length; i++) {
+            if (searchText.equals(products[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
