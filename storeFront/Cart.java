f149d4ff5011af21ff0aa0e0c780d351820a60c7
easonMT <t.eason@lafilm.edu>
2023-07-09 21:53:29 -0400

    cleared the cart class. starting fresh

--- storeFront/Cart.java
+++ storeFront/Cart.java
@@ -1,36 +1,8 @@
 package storeFront;
 
-import java.util.ArrayList;
 
 public class Cart {
-    
-    private ArrayList<Product> items = new ArrayList<>();
 
-    public void showItems(){
-        System.out.println("Items in Cart:");
-        for (Product item : items) {
-            System.out.println(item.getName() + " - $" + item.getPrice());
-        }
-
-    }
-
-    public void addItem(Product p) {
-        items.add(p);
-
-    }
-
-    public void removeItem(Product p) {
-        items.remove(p);
-    }
-
-    public double calculateTotal(){
-        double totalPrice = 0.0;
-        for (Product itemPrice : items) {
-            totalPrice += itemPrice.getPrice();
-        }
-        return totalPrice;
-
-    }
 
 
 }
