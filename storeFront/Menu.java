62f582b4cdd594d322e585eee3449b4fa6ce679b
easonMT <t.eason@lafilm.edu>
2023-07-09 21:37:23 -0400

    added instance variables and methods to the menu class

--- storeFront/Menu.java
+++ storeFront/Menu.java
@@ -1,5 +1,52 @@
 package storeFront;
 
+import java.util.Scanner;
+
 public class Menu {
-    
+    private String[] menuOptions = {"Exit","List Products","Buy Product","Find Product","Show Cart", "Checkout"};
+    private String[] products = {"Crew Neck", "V-Neck", "Polo", "Boat Neck", "Tank Top"};
+    private Scanner scanner;
+
+    public Menu(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void executeMenu() {
+        printMenu();
+        int choice = getNextIntFromUser();
+        while(choice != 0) {
+
+        }
+    }
+
+    public void greet(){
+
+    }
+
+    private void printProducts() {
+
+    }
+
+    public int findProduct(String searchText) {
+        return -1;
+    }
+
+    private void printMenu() {
+
+    }
+
+    private void exit() {
+
+    }
+
+    private int getNextIntFromUser() {
+        return scanner.nextInt();
+    }
+
+    private String getNextStringLineFromUser() {
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
+
+
 }
