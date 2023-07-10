2912ff3b957b5c0e20fe46450933e6e4ad7ee100
easonMT <t.eason@lafilm.edu>
2023-07-09 22:54:15 -0400

    changed the string array to an array of type products and listed the array differently for a cleaner look

--- storeFront/ShopRunner.java
+++ storeFront/ShopRunner.java
@@ -7,7 +7,13 @@ import java.util.Scanner;
 public class ShopRunner {
     
     public static void main(String[] args) {
-        String[] products = {"Crew Neck", "V-Neck", "Polo", "Boat Neck", "Tank Top"};
+        Product[] products = new Product[5];
+        products[0] = new Product("Crew Neck", 20.00); 
+        products[1] = new Product ("V-Neck", 20.00);
+        products[2] = new Product("Polo", 25.50);
+        products[3] = new Product ("Boat Neck", 30.25);
+        products[4] = new Product("Tank Top", 15.75);
+
         // Menu menu = new Menu(new Scanner(new InputStreamReader(System.in, Charset.forName("UTF-8"))));
         Menu menu = new Menu(new Scanner(System.in), new Shop(products));
 
