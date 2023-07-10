8d4a1d476eaf2a554083254176669d34cbc6fdbe
easonMT <t.eason@lafilm.edu>
2023-07-09 21:38:41 -0400

    erased shop class

--- storeFront/Shop.java
+++ storeFront/Shop.java
@@ -1,28 +1,6 @@
 package storeFront;
 
-import  java.util.ArrayList;
 
 public class Shop {
-
-    private ArrayList<Product> products = new ArrayList<>();
-
-
-    public void printProducts() {
-        products.add(new Product(0, "Crew Neck", 25.99));
-        products.add(new Product(1, "Tank Top", 15.99));
-        products.add(new Product(2, "Polo", 25.99));
-        products.add(new Product(3, "Graphic Tee", 17.99));
-        products.add(new Product(4, "Collar Shirt", 35.99));
-    }
-
-    public int findProduct() {
-        for (int i = 0; i < products.size(); i++) {
-            System.out.println(i);
-        }
-    }
-
-    public Product getProductById(int id) {
-        return null;
-    }
 
 }
