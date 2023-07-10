1bbff009aadb6dd6b0bf3cb7bc3102b4b4b1c93b
easonMT <t.eason@lafilm.edu>
2023-07-09 22:53:35 -0400

    adjusted the output of print products and updated find products method

--- storeFront/Shop.java
+++ storeFront/Shop.java
@@ -2,22 +2,22 @@ package storeFront;
 
 
 public class Shop {
-    public String[] products;
+    private Product[] products;
 
-    public Shop(String[] products) {
+    public Shop(Product[] products) {
         this.products = products.clone();
     }
 
         public void printProducts() {
         System.out.println("--Products--");
         for (int i = 0; i < products.length; i++) {
-            System.out.println("ID " +i+": " + products[i]);
+            System.out.println("ID " +i+": " + products[i].getName() + " - $" + products[i].getPrice());
         }
     }
 
     public int findProduct(String searchText) {
         for (int i = 0; i < products.length; i++) {
-            if (searchText.equals(products[i])) {
+            if (searchText.equals(products[i].getName())) {
                 return i;
             }
         }
