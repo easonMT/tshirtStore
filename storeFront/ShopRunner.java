350ab8a2ab8dbd109783e80e64bca102e460f6f8
easonMT <t.eason@lafilm.edu>
2023-07-09 22:01:47 -0400

    i adjusted the menu constructor in ShopRunner to attempt to simplify my code

--- storeFront/ShopRunner.java
+++ storeFront/ShopRunner.java
@@ -1,13 +1,14 @@
 package storeFront;
 
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+// import java.io.InputStreamReader;
+// import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class ShopRunner {
     
     public static void main(String[] args) {
-        Menu menu = new Menu(new Scanner(new InputStreamReader(System.in, Charset.forName("UTF-8"))));
+        // Menu menu = new Menu(new Scanner(new InputStreamReader(System.in, Charset.forName("UTF-8"))));
+        Menu menu = new Menu(new Scanner(System.in));
 
         menu.greet();
         menu.executeMenu();
