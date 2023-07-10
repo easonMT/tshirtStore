e02d6de774114bff5e20979b0c57815146b95442
easonMT <t.eason@lafilm.edu>
2023-07-09 21:39:10 -0400

    added import statements and main method to shop runner class

--- storeFront/ShopRunner.java
+++ storeFront/ShopRunner.java
@@ -1,5 +1,16 @@
 package storeFront;
 
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class ShopRunner {
     
+    public static void main(String[] args) {
+        Menu menu = new Menu(new Scanner(new InputStreamReader(System.in, Charset.forName("UTF-8"))));
+
+        menu.greet();
+        menu.executeMenu();
+    }
+
 }
